344c36dff8dd808bf76df8168785d5d14dd48244
822z495vbv-gif <822z495vbv@privaterelay.appleid.com>
2026-09-09 19:06:02 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`XGFX Bot is online as ${client.user.tag}`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("🏓 Pong! XGFX Bot is online!");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
